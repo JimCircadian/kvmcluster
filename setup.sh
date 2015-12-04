6ca2ca63d33924d6cf3ca0e026504567f85a4b2f
James Byrne <james.byrne@linuxit.com>
2015-12-04 12:26:00 +0000

    Got fresh build for CentOS 6 / Ruby 1.9.3

--- setup.sh
+++ setup.sh
@@ -6,16 +6,18 @@ if [ $UID -ne 0 ]; then
 fi
 
 yum -y install centos-release-SCL
-rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-yum -y install puppet hiera ruby193 ruby193-rubygems
-sed -ri 's/enabled=1/enabled=0/g' /etc/yum.repos.d/puppetlabs.repo
+yum -y install ruby193 ruby193-rubygems ruby193-ruby-devel gcc kernel-headers
+echo "source /opt/rh/ruby193/enable" | sudo tee -a /etc/profile.d/ruby193.sh
+chmod +x /etc/profile.d/ruby193.sh
+/etc/profile.d/ruby193.sh 2>&1 >/dev/null
 
-# gem install hiera -v 1.3.4
-# gem install puppet -v 3.8.4
+gem install hiera -v 1.3.4
+gem install puppet -v 3.8.4
 gem install kafo
 gem install librarian-puppet
 
 cd cluster-installer
+export PATH=/opt/rh/ruby193/root/usr/local/bin:$PATH
 librarian-puppet install
 
 if [ ! -f ~/answers.yaml ]; then
